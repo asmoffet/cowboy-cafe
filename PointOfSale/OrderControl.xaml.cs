799f0280da31821b1ab2da441218f5ea67a794ea
asmoffet <54548893+asmoffet@users.noreply.github.com>
2020-02-23 16:27:25 -0600

    diagrams and comments

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -32,77 +32,137 @@ namespace PointOfSale
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Adds angry chicken to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddAngryChickenButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new AngryChicken());
         }
-
+        /// <summary>
+        /// adds pecos pulled pork to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddPecosPulledPorkButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new PecosPulledPork());
         }
-
+        /// <summary>
+        /// adds cowpoke chilli to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddCowpokeChilliButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new CowpokeChili());
         }
-
+        /// <summary>
+        /// adds dakota double burger to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddDakotaDoubleBugrerButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new DakotaDoubleBurger());
         }
-
+        /// <summary>
+        /// adds baked beans to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddBakedBeansButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new BakedBeans());
         }
-
+        /// <summary>
+        /// adds chili cheese fries to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddChilliCheeseFriesButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new ChiliCheeseFries());
         }
-
+        /// <summary>
+        /// adds cord doddgers to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddCornDodgersButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new CornDodgers());
         }
-
+        /// <summary>
+        /// adds pan de campo to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddPanDeCampoButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new PanDeCampo());
         }
-
+        /// <summary>
+        /// adds cowboy coffee to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddCowboyCoffeeButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new CowboyCoffee());
         }
-
+        /// <summary>
+        /// adds jerked soda to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddJerkedSodaButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new JerkedSoda());
         }
-
+        /// <summary>
+        /// adds texas tea to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddTexasTeaButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new TexasTea());
         }
-
+        /// <summary>
+        /// adds water to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddWaterButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new Water());
         }
-
+        /// <summary>
+        /// adds rustlers ribs to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddRustlersRibsButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new RustlersRibs());
         }
-
+        /// <summary>
+        /// adds texas triple burger to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddTexasTripleBurgerButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new TexasTripleBurger());
         }
-
+        /// <summary>
+        /// adds trail burger to the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddTrailBurgeButton_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new TrailBurger());
