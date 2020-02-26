401771812f7900459c638b95bd5e8a1577b0e18a
asmoffet <asmoffet@ksu.edu>
2020-02-26 16:47:41 -0600

    and we can test now

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public interface IOrderItem
+    {
+        List<string> specialInstructions { get; }
+        double Price { get; }
+    }
+}
---
+++ DataTests/UnitTests/OrderTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CowboyCafe.Data;
+
+namespace CowboyCafe.DataTests
+{
+    public class OrderTests
+    {
+        public class MockOrderItem : IOrderItem
+        {
+            public double Price { get; set; }
+
+            public List<string> specialInstructions { get; set; }
+        }
+        [Fact]
+        public void ShouldBeAbleToAddItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.Contains(item, order.Items);
+            
+        }
+        [Fact]
+        public void ShouldBeAbleToRemoveItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            order.Remove(item);
+            Assert.DoesNotContain(item, order.Items);
+        }
+        [Fact]
+        public void ShouldBeAbleToGetTheEnumerationOfItems()
+        {
+            var order = new Order();
+            var item1 = new MockOrderItem();
+            var item2 = new MockOrderItem();
+            var item3 = new MockOrderItem();
+            var item4 = new MockOrderItem();
+            order.Add(item1);
+            order.Add(item2);
+            order.Add(item3);
+            order.Add(item4);
+
+            Assert.Collection(order.Items,
+                item => Assert.Equal(item1, item),
+                item => Assert.Equal(item2, item),
+                item => Assert.Equal(item3, item),
+                item => Assert.Equal(item4, item));
+        }
+        [Theory]
+        [InlineData(new double[] { 1, 2, 3 })]
+        [InlineData(new double[] { 0,0,0 })]
+        [InlineData(new double[] { 199,799 })]
+        [InlineData(new double[] { 789 })]
+        [InlineData(new double[] { } )]
+        [InlineData(new double[] { -5 })]
+        [InlineData(new double[] { 8, 10, -4 })]
+        [InlineData(new double[] { 3.12132 })]
+        [InlineData(new double[] { double.NaN })]
+        public void SubtotalShouldBeTheSumOfTheItems(double[] prices)
+        {
+            var order = new Order();
+            double total = 0;
+            foreach(var price in prices)
+            {
+                total += price;
+                order.Add(new MockOrderItem(){
+                    Price = price
+                });
+            }
+            Assert.Equal(total, order.Subtotal);
+        }
+        [Theory]
+        [InlineData("Items")]
+        [InlineData("Price")]
+        public void AddingAnItemShouldTriggerPropertyChanged(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Add(item);
+            });
+        }
+        [Theory]
+        [InlineData("Items")]
+        [InlineData("Price")]
+        public void RemovingAnItemShouldTriggerPropertyChanged(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Remove(item);
+            });
+        }
+
+    }
+}
