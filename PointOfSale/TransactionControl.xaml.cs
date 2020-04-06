b099feaf9789ebc888112e6a48086fa333a41305
asmoffet <54548893+asmoffet@users.noreply.github.com>
2020-04-06 16:10:43 -0500

    printing now!

--- PointOfSale/TransactionControl.xaml.cs
+++ PointOfSale/TransactionControl.xaml.cs
@@ -50,10 +50,19 @@ namespace PointOfSale
             
             string read = readCard.ProcessTransaction(totalPrice).ToString();
             if (read == "Success")
-            {
-                //print receipt
-                
+            {                
                 var ordercontrol = this.FindAncestor<OrderControl>();
+                string[] orderItems = new string[ordercontrol.OrderControlOrderSummaryControl.ListBoxSummary.Items.Count];
+                for(int i = 0; i < ordercontrol.OrderControlOrderSummaryControl.ListBoxSummary.Items.Count; i++)
+                {
+                    orderItems[i] = ordercontrol.OrderControlOrderSummaryControl.ListBoxSummary.Items[i].ToString();
+                }
+                int ordernumber = Convert.ToInt32(ordercontrol.OrderControlOrderSummaryControl.OrderNumber.ToString());
+                double subtotal = Convert.ToDouble(Sub.Text.ToString());
+
+                ReceiptPrinter rp = new ReceiptPrinter();
+                rp.Print(PrintCredit(totalPrice, subtotal, ordernumber, orderItems));
+
                 ordercontrol.SwapScreen(new MenuItemSelectionControl());
                 ordercontrol.DataContext = new Order();
             }
@@ -74,6 +83,36 @@ namespace PointOfSale
                 MessageBox.Show("Unkown Error");
             }
 
+        }
+
+        private string PrintCredit(double total, double subtotal, int orderNumber, string[] order)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format("Order Number: {0} \n",orderNumber));
+            sb.Append(String.Format("Date: {0}\n", DateTime.Now));
+            sb.Append("Items:\n");
+            foreach(string s in order)
+            {
+                sb.Append(s);
+                sb.Append("\n");
+            }
+            sb.Append(String.Format("Order Subtotal: {0} \nOrder Total: {1}", subtotal, total));
+            return sb.ToString();
+        }
+        private string PrintCash(double total, double subtotal, int orderNumber, string[] order, double cash)
+        {
+            StringBuilder sb = new StringBuilder();
+            double change = cash - total;
+            sb.Append(String.Format("Order Number: {0} \n", orderNumber));
+            sb.Append(String.Format("Date: {0}\n", DateTime.Now));
+            sb.Append("Items:\n");
+            foreach (string s in order)
+            {
+                sb.Append(s);
+                sb.Append("\n");
+            }
+            sb.Append(String.Format("Order Subtotal: {0} \nOrder Total: {1} \nChange: {2}", subtotal, total, change));
+            return sb.ToString();
         }
     }
 }
