5a3fa6a6ef8af0ea8d981f3de46614a733c6eb0f
asmoffet <54548893+asmoffet@users.noreply.github.com>
2020-04-27 17:48:10 -0500

    EVERYTHING WORKS! now todo unit tests

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace CowboyCafe.Data
 {
     public static class Menu
     {
+        /// <summary>
+        /// adds all the entrees to a list
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<IOrderItem> Entrees()
         {
             List<IOrderItem> entrees = new List<IOrderItem>();
@@ -18,7 +24,10 @@ namespace CowboyCafe.Data
             entrees.Add(new TrailBurger());
             return entrees;
         }
-
+        /// <summary>
+        /// adds all the sides to a list
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<IOrderItem> Sides()
         {
             List<IOrderItem> sides = new List<IOrderItem>();
@@ -28,7 +37,10 @@ namespace CowboyCafe.Data
             sides.Add(new PanDeCampo());
             return sides;
         }
-
+        /// <summary>
+        /// adds all the drinks to a list
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<IOrderItem> Drinks()
         {
             List<IOrderItem> drinks = new List<IOrderItem>();
@@ -38,7 +50,10 @@ namespace CowboyCafe.Data
             drinks.Add(new Water());
             return drinks;
         }
-
+        /// <summary>
+        /// makes a complete menu list
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<IOrderItem> CompleteMenu()
         {
             List<IOrderItem> menu = new List<IOrderItem>();
@@ -58,6 +73,187 @@ namespace CowboyCafe.Data
                 menu.Add(drink);
             }
             return menu;
+        }
+        /// <summary>
+        /// takes in a search term and returns a list of items with the terms in it.
+        /// </summary>
+        /// <param name="terms"></param>
+        /// <returns></returns>
+        public static IEnumerable<IOrderItem> Search(string terms)
+        {
+            List<IOrderItem> results = new List<IOrderItem>();
+            List<IOrderItem> all = new List<IOrderItem>(CompleteMenu());
+            if (terms == null) return CompleteMenu();
+
+            foreach(IOrderItem item in all)
+            {
+                if(item.ToString() != null && item.ToString().Contains(terms, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+        /// <summary>
+        /// filters the list by the type of item
+        /// </summary>
+        /// <param name="types">a array of different item types</param>
+        /// <returns></returns>
+        public static IEnumerable<IOrderItem> filterByType(IEnumerable<string> types, IEnumerable<IOrderItem> items)
+        {
+            if (types == null || types.Count() == 0) return items;
+
+            List<IOrderItem> results = new List<IOrderItem>();
+            if (types.Contains("entree"))
+            {
+                IEnumerable<IOrderItem> entrees = Entrees();
+                foreach (IOrderItem item in items)
+                {
+                    foreach(IOrderItem entree in entrees)
+                    {
+                        if (item.GetType() == entree.GetType())
+                        {
+                            results.Add(entree);
+                        }
+                    } 
+                }
+            }
+
+            if (types.Contains("side"))
+            {
+                IEnumerable<IOrderItem> sides = Sides();
+                foreach (IOrderItem item in items)
+                {
+                    foreach (IOrderItem side in sides)
+                    {
+                        if (item.GetType() == side.GetType())
+                        {
+                            results.Add(side);
+                        }
+
+                    }
+                }
+                
+            }
+            if (types.Contains("drink"))
+            {
+                IEnumerable<IOrderItem> drinks = Drinks();
+                foreach (IOrderItem item in items)
+                {
+                    foreach (IOrderItem drink in drinks)
+                    {
+                        if (item.GetType() == drink.GetType())
+                        {
+                            results.Add(drink);
+                        }
+
+                    }
+                }
+            }
+            return results;
+        }
+        /// <summary>
+        /// filters the items by their price
+        /// </summary>
+        /// <param name="items">the list of items</param>
+        /// <param name="min">minimum price</param>
+        /// <param name="max">maximum price</param>
+        /// <returns></returns>
+        public static IEnumerable<IOrderItem> FilterItemsByPrice(IEnumerable<IOrderItem> items, double? min, double? max)
+        {
+
+            if (min == null && max == null)
+            {
+                return items;
+            }
+
+            List<IOrderItem> results = new List<IOrderItem>();
+
+            if (min == null)
+            {
+                foreach (IOrderItem item in items)
+                {
+                    if (item.Price <= max)
+                    {
+                        results.Add(item);
+                    }
+                }
+                return results;
+            }
+
+            if (max == null)
+            {
+                foreach (IOrderItem item in items)
+                {
+                    if (item.Price >= min)
+                    {
+                        results.Add(item);
+                    }
+                }
+                return results;
+            }
+
+            foreach (IOrderItem item in items)
+            {
+                if (item.Price >= min && item.Price <= max)
+                {
+                    results.Add(item);
+                }
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// filters the items by their price
+        /// </summary>
+        /// <param name="items">the list of items</param>
+        /// <param name="min">minimum Calories</param>
+        /// <param name="max">maximum Calories</param>
+        /// <returns></returns>
+        public static IEnumerable<IOrderItem> FilterItemsByCalories(IEnumerable<IOrderItem> items, int? min, int? max)
+        {
+
+            if (min == null && max == null)
+            {
+                return items;
+            }
+
+            List<IOrderItem> results = new List<IOrderItem>();
+
+            if (min == null)
+            {
+                foreach (IOrderItem item in items)
+                {
+                    if (item.Calories <= max)
+                    {
+                        results.Add(item);
+                    }
+                }
+                return results;
+            }
+
+            if (max == null)
+            {
+                foreach (IOrderItem item in items)
+                {
+                    if (item.Calories >= min)
+                    {
+                        results.Add(item);
+                    }
+                }
+                return results;
+            }
+
+            foreach (IOrderItem item in items)
+            {
+                if (item.Calories >= min && item.Calories <= max)
+                {
+                    results.Add(item);
+                }
+            }
+
+            return results;
         }
     }
 }
--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using CowboyCafe.Data;
 
 namespace Website.Pages
 {
@@ -16,10 +17,49 @@ namespace Website.Pages
         {
             _logger = logger;
         }
+        /// <summary>
+        /// a list of the IOrder Items to be displayed
+        /// </summary>
+        public IEnumerable<IOrderItem> Items { get; protected set; }
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; }
+        /// <summary>
+        /// the minimum desired price
+        /// </summary>
+        public double? priceMin { get; set; }
+        /// <summary>
+        /// the maximum desired price
+        /// </summary>
+        public double? priceMax { get; set; }
+        /// <summary>
+        /// the minimum desired calories
+        /// </summary>
+        public int? calMin { get; set; }
+        /// <summary>
+        /// the maximum desired calories
+        /// </summary>
+        public int? calMax { get; set; }
 
-        public void OnGet()
+        public string[] types { get; set; }
+
+        public void OnGet(double? priceMin, double? priceMax, int? calMin, int? calMax)
         {
+            SearchTerms = Request.Query["SearchTerms"];
+            types = Request.Query["type"];
+            //nullable conversion workaround from Razorpage project
+            this.priceMax = priceMax;
+            this.priceMin = priceMin;
+            this.calMax = calMax;
+            this.calMin = calMin;
 
+            Items = Menu.Search(SearchTerms);
+            Items = Menu.filterByType(types, Items);
+            Items = Menu.FilterItemsByPrice(Items, priceMin, priceMax);
+            Items = Menu.FilterItemsByCalories(Items, calMin, calMax);
+            
         }
     }
 }
